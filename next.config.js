dcb29229031c09524b7e195943e628d70cc795e8
wacome <twk1319332366@gmail.com>
2025-07-26 14:24:36 +0800

    modify ts to js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
-// next.config.ts
-
-import { NextConfig } from 'next';
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   // 1. 严格模式 (React Strict Mode)
   reactStrictMode: true,
 
@@ -11,19 +8,18 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'blog.toycon.cn', // 允许您自己博客的图片
+        hostname: 'blog.toycon.cn',
         port: '',
-        pathname: '/uploads/**', // 只允许 /uploads/ 路径下的图片
+        pathname: '/uploads/**',
       },
       {
         protocol: 'https',
-        hostname: 'avatars.githubusercontent.com', // 允许 GitHub 头像
+        hostname: 'avatars.githubusercontent.com',
       },
       {
         protocol: 'https',
         hostname: 'cdn.jsdelivr.net', // 允许 jsDelivr CDN
       },
-
       {
         protocol: 'https',
         hostname: 'blog-1257292087.cos.ap-nanjing.myqcloud.com',
@@ -33,7 +29,7 @@ const nextConfig: NextConfig = {
 
   // 3. 环境变量 (Environment Variables)
   env: {
-    API_BASE_URL: process.env.API_BASE_URL,
+    API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
   },
 
   // 4. 在生产环境中自动移除 console.log
@@ -44,8 +40,8 @@ const nextConfig: NextConfig = {
   // 5. 启用 SWC 压缩
   swcMinify: true,
 
-  // 6. 独立的构建输出目录 (关键！)
+  // 6. 独立的构建输出目录 (解决文件过大问题的关键！)
   output: 'standalone',
 };
 
-export default nextConfig;
+module.exports = nextConfig;
